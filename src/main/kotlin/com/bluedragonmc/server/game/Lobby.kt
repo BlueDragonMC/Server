0cde0a9efddbc67fa2ad48cf6a34a19b88cf8d34
tylerswanson2 <31103658+tylerswanson2@users.noreply.github.com>
2022-07-13 21:17:26 -0400

    Enable Mojang authentication, add player XP system

--- src/main/kotlin/com/bluedragonmc/server/game/Lobby.kt
+++ src/main/kotlin/com/bluedragonmc/server/game/Lobby.kt
@@ -1,7 +1,9 @@
 package com.bluedragonmc.server.game
 
 import com.bluedragonmc.messages.GameType
+import com.bluedragonmc.server.CustomPlayer
 import com.bluedragonmc.server.Game
+import com.bluedragonmc.server.module.GameModule
 import com.bluedragonmc.server.module.gameplay.*
 import com.bluedragonmc.server.module.instance.SharedInstanceModule
 import com.bluedragonmc.server.module.map.AnvilFileMapProviderModule
@@ -9,9 +11,16 @@ import com.bluedragonmc.server.queue
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
+import net.minestom.server.MinecraftServer
 import net.minestom.server.coordinate.Pos
 import net.minestom.server.entity.GameMode
+import net.minestom.server.entity.Player
+import net.minestom.server.event.Event
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.player.PlayerSpawnEvent
+import net.minestom.server.timer.Task
 import java.nio.file.Paths
+import java.time.Duration
 
 class Lobby : Game("Lobby", "lobbyv2.1") {
     init {
@@ -20,7 +29,7 @@ class Lobby : Game("Lobby", "lobbyv2.1") {
         use(SharedInstanceModule())
 
         // Player modules
-        use(VoidDeathModule(32.0))
+        use(VoidDeathModule(32.0, respawnMode = true))
         use(InstantRespawnModule())
         use(SpawnpointModule(SpawnpointModule.SingleSpawnpointProvider(Pos(0.5, 64.0, 0.5, 180F, 0F))))
         use(PlayerResetModule(defaultGameMode = GameMode.ADVENTURE))
@@ -48,6 +57,26 @@ class Lobby : Game("Lobby", "lobbyv2.1") {
                 })
         }
 
+        use(object : GameModule() {
+            override fun initialize(parent: Game, eventNode: EventNode<Event>) {
+                eventNode.addListener(PlayerSpawnEvent::class.java) { event ->
+                    loadXPBar(event.player)
+                }
+            }
+
+        })
         ready()
+    }
+
+    fun loadXPBar(player: Player) {
+        lateinit var loadXPTask: Task
+        loadXPTask = MinecraftServer.getSchedulerManager().buildTask {
+            val player = player as CustomPlayer
+            if (!player.isDataInitialized()) return@buildTask
+            val playerXP = player.data.experience
+            player.exp = CustomPlayer.getXpPercent(playerXP)
+            player.level = CustomPlayer.getXpLevel(playerXP)
+            loadXPTask.cancel()
+        }.repeat(Duration.ofMillis(20)).schedule()
     }
 }
