f47d97250f8ab0623c0e269ce6ece881024d8bf7
tylerswanson2 <31103658+tylerswanson2@users.noreply.github.com>
2022-07-13 21:16:45 -0400

    Add /party accept, warp, transfer

--- src/main/kotlin/com/bluedragonmc/server/command/PartyCommand.kt
+++ src/main/kotlin/com/bluedragonmc/server/command/PartyCommand.kt
@@ -1,8 +1,6 @@
 package com.bluedragonmc.server.command
 
-import com.bluedragonmc.messages.InvitePlayerToPartyMessage
-import com.bluedragonmc.messages.PartyChatMessage
-import com.bluedragonmc.messages.RemovePlayerFromPartyMessage
+import com.bluedragonmc.messages.*
 import com.bluedragonmc.server.module.messaging.MessagingModule
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
@@ -38,6 +36,26 @@ class PartyCommand(name: String, usage: String, vararg aliases: String) : BlueDr
         syntax(chatArgument) {
             val component = MiniMessage.miniMessage().deserialize("<msg>", Placeholder.unparsed("msg", get(chatArgument).joinToString(separator = " ")))
             MessagingModule.publish(PartyChatMessage(player.uuid, MiniMessage.miniMessage().serialize(component)))
+        }
+    }
+
+    subcommand("accept") {
+        val playerArgument by PlayerArgument
+        syntax(playerArgument) {
+            MessagingModule.publish(AcceptPartyInvitationMessage(getFirstPlayer(playerArgument).uuid, player.uuid))
+        }
+    }
+
+    subcommand("warp") {
+        syntax {
+            MessagingModule.publish(PartyWarpMessage(player.uuid, MessagingModule.containerId, player.instance!!.uniqueId))
+        }
+    }
+
+    subcommand("transfer") {
+        val playerArgument by PlayerArgument
+        syntax(playerArgument) {
+            MessagingModule.publish(PartyTransferMessage(player.uuid, getFirstPlayer(playerArgument).uuid))
         }
     }
 
