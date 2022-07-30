db8b9fb8f4c8134c2b0755fb97b0411f4b4b2d42
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-07-30 15:50:18 -0400

    Packet-based entity glowing without globally registering teams

--- src/main/kotlin/com/bluedragonmc/server/utils/packet/GlowingEntityUtils.kt
+++ src/main/kotlin/com/bluedragonmc/server/utils/packet/GlowingEntityUtils.kt
@@ -1,33 +1,33 @@
 package com.bluedragonmc.server.utils.packet
 
-import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
 import net.minestom.server.MinecraftServer
+import net.minestom.server.adventure.audience.PacketGroupingAudience
 import net.minestom.server.entity.Entity
+import net.minestom.server.entity.Player
+import net.minestom.server.network.packet.server.play.TeamsPacket
 import net.minestom.server.scoreboard.Team
 import net.minestom.server.tag.Tag
 import java.util.*
 
 object GlowingEntityUtils {
 
-    private val TEAM_NAME_TAG = Tag.String("entity_team_name")
+    private val SEEN_TEAMS_TAG = Tag.String("seen_teams").list()
     private val teamCache = mutableMapOf<NamedTextColor, Team>()
 
-    fun glow(entity: Entity, color: NamedTextColor) {
+    fun glow(entity: Entity, color: NamedTextColor, viewers: Collection<Player>) {
         entity.isGlowing = true
         val team = teamCache.getOrPut(color) {
-            MinecraftServer.getTeamManager().createTeam(
-                UUID.randomUUID().toString(), Component.empty(), Component.empty(), color, Component.empty()
-            )
+            MinecraftServer.getTeamManager().createBuilder(UUID.randomUUID().toString()).teamColor(color).build()
         }
-        val uuid = entity.uuid.toString()
-        if (entity.hasTag(TEAM_NAME_TAG)) {
-            val currentTeam = MinecraftServer.getTeamManager().getTeam(entity.getTag(TEAM_NAME_TAG))
-            if (currentTeam != team) currentTeam.removeMember(uuid)
-        }
-        if (!team.members.contains(uuid)) {
-            team.addMember(uuid)
-            entity.setTag(TEAM_NAME_TAG, team.teamName)
-        }
+
+        // Send the team creation packet to all viewers that have not been sent this packet before.
+        PacketGroupingAudience.of(viewers.filter {
+            !it.hasTag(SEEN_TEAMS_TAG) || !it.getTag(SEEN_TEAMS_TAG)!!.contains(team.teamName)
+        }).sendGroupedPacket(team.createTeamsCreationPacket())
+
+        PacketGroupingAudience.of(viewers).sendGroupedPacket(
+            TeamsPacket(team.teamName, TeamsPacket.AddEntitiesToTeamAction(listOf(entity.uuid.toString())))
+        )
     }
 }
