6c44e624f09d746629c76bd1866c6bfaa0c60852
tylerswanson2 <31103658+tylerswanson2@users.noreply.github.com>
2022-07-31 09:39:14 -0400

    Improvements to MOTD module

--- src/main/kotlin/com/bluedragonmc/server/module/gameplay/MOTDModule.kt
+++ src/main/kotlin/com/bluedragonmc/server/module/gameplay/MOTDModule.kt
@@ -7,9 +7,11 @@ import com.bluedragonmc.server.module.GameModule
 import com.bluedragonmc.server.module.database.DatabaseModule
 import com.bluedragonmc.server.module.database.MapData
 import com.bluedragonmc.server.utils.hoverEvent
+import com.bluedragonmc.server.utils.noBold
 import com.bluedragonmc.server.utils.surroundWithSeparators
 import kotlinx.coroutines.launch
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.HoverEvent
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.format.TextDecoration
 import net.minestom.server.event.Event
@@ -35,7 +37,10 @@ class MOTDModule(val motd: Component) : GameModule() {
                 Component.text(parent.name + "\n", BRAND_COLOR_PRIMARY_1, TextDecoration.BOLD)
                     .append(motd.color(NamedTextColor.WHITE).decoration(TextDecoration.BOLD, false))
                     .append(Component.text("\nMap: ", BRAND_COLOR_PRIMARY_2).decoration(TextDecoration.BOLD, false))
-                    .append(Component.text(parent.mapName, BRAND_COLOR_PRIMARY_1, TextDecoration.BOLD).hoverEvent(mapData.description, NamedTextColor.WHITE))
+                    .append(Component.text(parent.mapName, BRAND_COLOR_PRIMARY_1, TextDecoration.BOLD)
+                        .hoverEvent(HoverEvent.showText(Component.text(parent.mapName, BRAND_COLOR_PRIMARY_1, TextDecoration.BOLD)
+                            .append(Component.newline())
+                            .append(Component.text(mapData.description, NamedTextColor.GRAY).noBold()))))
                     .append(Component.text(" by ", BRAND_COLOR_PRIMARY_2).decoration(TextDecoration.BOLD, false))
                     .append(Component.text(mapData.author, BRAND_COLOR_PRIMARY_1).decoration(TextDecoration.BOLD, false))
                     .surroundWithSeparators()
