cdf64080700fa28ef318f90b51b8126479f16756
tylerswanson2 <31103658+tylerswanson2@users.noreply.github.com>
2022-09-02 22:13:07 -0400

    Improvements to JukeboxMenuBlockHandler.kt

--- src/main/kotlin/com/bluedragonmc/server/block/JukeboxMenuBlockHandler.kt
+++ src/main/kotlin/com/bluedragonmc/server/block/JukeboxMenuBlockHandler.kt
@@ -4,12 +4,16 @@ import com.bluedragonmc.server.NAMESPACE
 import com.bluedragonmc.server.module.GuiModule
 import com.bluedragonmc.server.utils.displayName
 import com.bluedragonmc.server.utils.noItalic
+import net.kyori.adventure.sound.Sound
 import net.kyori.adventure.text.Component
+import net.minestom.server.MinecraftServer
 import net.minestom.server.effects.Effects
+import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.instance.Instance
 import net.minestom.server.instance.block.BlockHandler
 import net.minestom.server.inventory.InventoryType
 import net.minestom.server.item.Material
+import net.minestom.server.network.packet.server.play.StopSoundPacket
 import net.minestom.server.utils.NamespaceID
 
 /**
@@ -17,29 +21,45 @@ import net.minestom.server.utils.NamespaceID
  * When the player chooses a song, it is played to everyone around them.
  */
 class JukeboxMenuBlockHandler(val instance: Instance, val x: Int, val y: Int, val z: Int) : BlockHandler {
-        private val guiModule = GuiModule()
-        val menu = guiModule.createMenu(
-            title = Component.translatable("lobby.menu.jukebox.title"),
-            inventoryType = InventoryType.CHEST_3_ROW,
-            isPerPlayer = false,
-            allowSpectatorClicks = false
-        ) {
-            val materials = Material.values()
-            var i = 0
-            materials.forEach { disc ->
-                if (!disc.name().startsWith("minecraft:music_disc_")) return@forEach
-                this.slot(i++, disc, { player ->
-                    displayName(disc.displayName().noItalic())
-                }) {
-                    instance.players.forEach { it.playEffect(Effects.PLAY_RECORD, x, y, z, disc.id(), false) }
-                    menu.close(player)
-                }
-            }
-        }
-
-        override fun getNamespaceId(): NamespaceID = NamespaceID.from("$NAMESPACE:jukebox")
-        override fun onInteract(interaction: BlockHandler.Interaction): Boolean {
-            menu.open(interaction.player)
-            return false
+    init {
+        MinecraftServer.getGlobalEventHandler().addListener(PlayerSpawnEvent::class.java) { event ->
+            if (mostRecentSong != null) event.player.sendPacket(
+                StopSoundPacket(
+                    0,
+                    Sound.Source.BLOCK,
+                    mostRecentSong
+                )
+            )
         }
     }
+
+    private var mostRecentSong: String? = null
+    private val guiModule = GuiModule()
+    val menu = guiModule.createMenu(
+        title = Component.translatable("lobby.menu.jukebox.title"),
+        inventoryType = InventoryType.CHEST_3_ROW,
+        isPerPlayer = false,
+        allowSpectatorClicks = false
+    ) {
+        val materials = Material.values()
+        val discs = materials.filter { it.name().startsWith("minecraft:music_disc_") }.sortedBy { it.name() }
+        discs.forEachIndexed { i, disc ->
+            this.slot(i, disc, { player ->
+                displayName(disc.displayName().noItalic())
+            }) {
+                instance.players.forEach { it.playEffect(Effects.PLAY_RECORD, x, y, z, disc.id(), false) }
+                mostRecentSong = discToSound(disc.name())
+                menu.close(player)
+            }
+        }
+    }
+
+    override fun getNamespaceId(): NamespaceID = NamespaceID.from("$NAMESPACE:jukebox")
+    override fun onInteract(interaction: BlockHandler.Interaction): Boolean {
+        menu.open(interaction.player)
+        return false
+    }
+
+    fun discToSound(discName: String): String =
+        "minecraft:music_disc.${discName.substringAfter("minecraft:music_disc_")}"
+}
