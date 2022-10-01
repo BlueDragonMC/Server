bb54845630989d5ff48315b94e3800f8e0bd82ee
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-09-30 20:42:13 -0400

    Fix some issues with duplicate keys in PermissionCommand.

--- src/main/kotlin/com/bluedragonmc/server/command/PermissionCommand.kt
+++ src/main/kotlin/com/bluedragonmc/server/command/PermissionCommand.kt
@@ -18,7 +18,7 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
     val value by BooleanArgument
 
     subcommand("group") {
-        val groupArgument by PermissionGroupArgument
+        val permissionGroup by PermissionGroupArgument
 
         val add by LiteralArgument
         val delete by LiteralArgument
@@ -46,10 +46,10 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", groupName))
             }
         }
-        suspendSyntax(groupArgument, info) { // permission group <groupName> info
-            val group = Permissions.getGroupByName(get(groupArgument))
+        suspendSyntax(permissionGroup, info) { // permission group <groupName> info
+            val group = Permissions.getGroupByName(get(permissionGroup))
             if(group == null) {
-                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(groupArgument)))
+                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(permissionGroup)))
                 return@suspendSyntax
             }
             sender.sendMessage(buildMessage {
@@ -63,10 +63,10 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 // todo display group name, color, prefix, suffix, etc.
             })
         }
-        suspendSyntax(groupArgument, permission, get, value) { // permission group <groupName> permission get <node>
-            val group = Permissions.getGroupByName(get(groupArgument))
+        suspendSyntax(permissionGroup, permission, get, value) { // permission group <groupName> permission get <node>
+            val group = Permissions.getGroupByName(get(permissionGroup))
             if(group == null) {
-                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(groupArgument)))
+                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(permissionGroup)))
                 return@suspendSyntax
             }
             val result = group.permissions.contains(get(permission))
@@ -76,26 +76,26 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 sender.sendMessage(formatMessageTranslated("command.permission.group.permission.get.false", group.name, get(node)))
             }
         }
-        suspendSyntax(groupArgument, permission, set, node, value) {
-            val group = Permissions.getGroupByName(get(groupArgument))
+        suspendSyntax(permissionGroup, permission, set, node, value) {
+            val group = Permissions.getGroupByName(get(permissionGroup))
             if(group == null) {
-                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(groupArgument)))
+                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(permissionGroup)))
                 return@suspendSyntax
             }
             Permissions.setPermission(group, get(node), get(value))
             sender.sendMessage(formatMessageTranslated("command.permission.group.permission.set", get(node), group.name, get(value)))
         }
-        suspendSyntax(groupArgument, permission, unset, node) {
-            val group = Permissions.getGroupByName(get(groupArgument))
+        suspendSyntax(permissionGroup, permission, unset, node) {
+            val group = Permissions.getGroupByName(get(permissionGroup))
             if(group == null) {
-                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(groupArgument)))
+                sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", get(permissionGroup)))
                 return@suspendSyntax
             }
             Permissions.removePermission(group, get(node))
             sender.sendMessage(formatMessageTranslated("command.permission.group.permission.unset", get(node), group.name, get(value)))
         }
-        suspendSyntax(groupArgument, permission, info) { // permission group <groupName> permission info
-            val group = get(groupArgument)
+        suspendSyntax(permissionGroup, permission, info) { // permission group <groupName> permission info
+            val group = get(permissionGroup)
             val perms = Permissions.getGroupByName(group)?.permissions
             if(perms == null) {
                 sender.sendMessage(formatErrorTranslated("command.permission.group.does_not_exist", group))
@@ -113,9 +113,9 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
     }
 
     subcommand("user") {
-        val userArgument by OfflinePlayerArgument
+        val playerName by OfflinePlayerArgument
 
-        userSuspendSyntax(userArgument, permission, get, node) { // permission user <player> permission get <node>
+        userSuspendSyntax(playerName, permission, get, node) { // permission user <player> permission get <node>
             val result = Permissions.getPermission(doc, get(node))
             if(result) {
                 sender.sendMessage(formatMessageTranslated("command.permission.user.permission.get.true", doc.username, get(node)))
@@ -123,12 +123,12 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 sender.sendMessage(formatMessageTranslated("command.permission.user.permission.get.false", doc.username, get(node)))
             }
         }
-        userSuspendSyntax(userArgument, permission, set, node, value) { // permission user <player> permission set <node> <true|false>
+        userSuspendSyntax(playerName, permission, set, node, value) { // permission user <player> permission set <node> <true|false>
             val node = get(node)
             Permissions.setPermission(doc, node, get(value))
             sender.sendMessage(formatMessageTranslated("command.permission.user.permission.set", node, doc.username, get(value))) // TODO negated (false) permissions
         }
-        userSuspendSyntax(userArgument, permission, unset, node) { // permission user <player> permission unset <node>
+        userSuspendSyntax(playerName, permission, unset, node) { // permission user <player> permission unset <node>
             val node = get(node)
             if (Permissions.getPermission(doc, node)) {
                 Permissions.removePermission(doc, node)
@@ -137,7 +137,7 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 sender.sendMessage(formatErrorTranslated("command.permission.user.permission.unset.fail", doc.username, node))
             }
         }
-        userSuspendSyntax(userArgument, permission, info) { // permission user <player> permission info
+        userSuspendSyntax(playerName, permission, info) { // permission user <player> permission info
             val perms = doc.getAllPermissions().toTypedArray()
             sender.sendMessage(buildMessage {
                 component(formatMessageTranslated("command.permission.user.permission.info", doc.username, perms.size))
@@ -155,7 +155,7 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
 
         val groupName by PermissionGroupArgument
 
-        userSuspendSyntax(userArgument, group, add, groupName) { // permission user <player> group add <groupName>
+        userSuspendSyntax(playerName, group, add, groupName) { // permission user <player> group add <groupName>
             val groupName = get(groupName)
             if(Permissions.isInGroup(doc, groupName)) {
                 sender.sendMessage(formatErrorTranslated("command.permission.user.group.add.fail", doc.username, groupName))
@@ -164,7 +164,7 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 sender.sendMessage(formatMessageTranslated("command.permission.user.group.add.success", doc.username, groupName))
             }
         }
-        userSuspendSyntax(userArgument, group, remove, groupName) { // permission user <player> group remove <groupName>
+        userSuspendSyntax(playerName, group, remove, groupName) { // permission user <player> group remove <groupName>
             val groupName = get(groupName)
             if(Permissions.isInGroup(doc, groupName)) {
                 Permissions.removeGroup(doc, groupName)
@@ -173,7 +173,7 @@ class PermissionCommand(name: String, usageString: String, vararg aliases: Strin
                 sender.sendMessage(formatErrorTranslated("command.permission.user.group.remove.fail", doc.username, groupName))
             }
         }
-        userSuspendSyntax(userArgument, group, info) { // permission user <player> group info
+        userSuspendSyntax(playerName, group, info) { // permission user <player> group info
             val groups = Permissions.getGroups(doc)
             sender.sendMessage(buildMessage {
                 component(formatMessageTranslated("command.permission.user.group.list", doc.username))
