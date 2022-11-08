0245e4c468ae0e816de33070967dbfda55113ac0
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-11-08 17:05:46 -0500

    Implement latest RPC spec

--- common/src/main/kotlin/com/bluedragonmc/server/module/messaging/MessagingModule.kt
+++ common/src/main/kotlin/com/bluedragonmc/server/module/messaging/MessagingModule.kt
@@ -1,9 +1,10 @@
 package com.bluedragonmc.server.module.messaging
 
 import com.bluedragonmc.api.grpc.*
+import com.bluedragonmc.api.grpc.GetInstancesResponseKt.runningInstance
+import com.bluedragonmc.api.grpc.GetPlayersResponseKt.connectedPlayer
 import com.bluedragonmc.api.grpc.GsClient.CreateInstanceResponse
 import com.bluedragonmc.api.grpc.PlayerHolderOuterClass.SendPlayerResponse.SuccessFlags
-import com.bluedragonmc.api.grpc.ServerSyncRequestKt.runningInstance
 import com.bluedragonmc.server.Environment
 import com.bluedragonmc.server.Game
 import com.bluedragonmc.server.event.GameStateChangedEvent
@@ -13,7 +14,6 @@ import com.bluedragonmc.server.module.database.DatabaseModule
 import com.bluedragonmc.server.module.instance.InstanceModule
 import com.bluedragonmc.server.module.messaging.MessagingModule.Stubs.gameStateSvcStub
 import com.bluedragonmc.server.module.messaging.MessagingModule.Stubs.instanceSvcStub
-import com.bluedragonmc.server.module.messaging.MessagingModule.Stubs.serverTrackingStub
 import com.bluedragonmc.server.utils.miniMessage
 import com.google.protobuf.Empty
 import io.grpc.*
@@ -29,7 +29,6 @@ import java.net.InetAddress
 import java.util.*
 import java.util.concurrent.TimeUnit
 import java.util.function.Consumer
-import kotlin.concurrent.timer
 import kotlin.system.exitProcess
 
 @DependsOn(InstanceModule::class)
@@ -44,10 +43,6 @@ class MessagingModule : GameModule() {
                 .usePlaintext()
                 .enableRetry()
                 .build()
-        }
-
-        val serverTrackingStub by lazy {
-            ServerTrackerServiceGrpcKt.ServerTrackerServiceCoroutineStub(channel)
         }
 
         val instanceSvcStub by lazy {
@@ -107,28 +102,12 @@ class MessagingModule : GameModule() {
                     logger.error("Severe error: failed to gather container ID from Environment.")
                     exitProcess(1)
                 }
-                serverTrackingStub.initGameServer(initGameServerRequest {
+                instanceSvcStub.initGameServer(initGameServerRequest {
                     serverName = this@Companion.serverName
                 })
                 logger.info("Published ping message.")
                 serverNameWaitingActions.forEach { it.accept(serverName) }
                 serverNameWaitingActions.clear()
-            }
-            // Start server sync process every 30 seconds
-            timer("server-sync", daemon = true, initialDelay = 30_000, period = 30_000) {
-                // Every 30 seconds, send a synchronization message
-                DatabaseModule.IO.launch {
-                    serverTrackingStub.serverSync(serverSyncRequest {
-                        serverName = this@Companion.serverName
-                        instances += Game.games.map {
-                            runningInstance {
-                                instanceUuid = it.instanceId.toString()
-                                gameType = it.gameType
-                                gameState = it.rpcGameState
-                            }
-                        }
-                    })
-                }
             }
             // Start a gRPC server for other services to call
             val port = 50051
@@ -233,6 +212,18 @@ class MessagingModule : GameModule() {
             }
             return Empty.getDefaultInstance()
         }
+
+        override suspend fun getInstances(request: Empty): GsClient.GetInstancesResponse {
+            return getInstancesResponse {
+                Game.games.filter { it.instanceId != null }.forEach { game ->
+                    instances += runningInstance {
+                        this.gameState = game.rpcGameState
+                        this.instanceUuid = game.instanceId!!.toString()
+                        this.gameType = game.gameType
+                    }
+                }
+            }
+        }
     }
 
     class PlayerHolderService : PlayerHolderGrpcKt.PlayerHolderCoroutineImplBase() {
@@ -240,6 +231,18 @@ class MessagingModule : GameModule() {
             Environment.current.queue.sendPlayer(request)
             return sendPlayerResponse {
                 successes += SuccessFlags.SET_INSTANCE
+            }
+        }
+
+        override suspend fun getPlayers(request: Empty): PlayerHolderOuterClass.GetPlayersResponse {
+            return getPlayersResponse {
+                MinecraftServer.getConnectionManager().onlinePlayers.forEach { player ->
+                    players += connectedPlayer {
+                        this.serverName = MessagingModule.serverName
+                        this.uuid = player.uuid.toString()
+                        this.username = player.username
+                    }
+                }
             }
         }
     }
