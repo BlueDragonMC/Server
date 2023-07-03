2659ad43eceaacf9a48f6de703fdcd777173dfa2
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-07-03 16:31:03 -0400

    Fix build

---
+++ src/main/kotlin/com/bluedragonmc/server/bootstrap/PerInstanceChat.kt
@@ -0,0 +1,24 @@
+package com.bluedragonmc.server.bootstrap
+
+import net.minestom.server.event.Event
+import net.minestom.server.event.EventNode
+import net.minestom.server.event.player.PlayerChatEvent
+import net.minestom.server.event.player.PlayerDeathEvent
+
+object PerInstanceChat : Bootstrap() {
+
+    override fun hook(eventNode: EventNode<Event>) {
+        eventNode.addListener(PlayerChatEvent::class.java) { event ->
+            // Restrict player chat messages to only be visible in the player's current instance.
+            event.recipients.removeAll { it.instance != event.player.instance }
+        }
+        eventNode.addListener(PlayerDeathEvent::class.java) { event ->
+            // Cancel the regular death message and only send it to the dead player's instance.
+            val message = event.chatMessage
+            event.chatMessage = null
+            if (message != null) {
+                event.instance.sendMessage(message)
+            }
+        }
+    }
+}
