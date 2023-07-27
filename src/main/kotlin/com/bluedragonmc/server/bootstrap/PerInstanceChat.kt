bced92db14bc79f4271a11b4be35a46e56c89075
tylerswanson2 <31103658+tylerswanson2@users.noreply.github.com>
2023-07-26 21:16:28 -0400

    Fix death messages

--- src/main/kotlin/com/bluedragonmc/server/bootstrap/PerInstanceChat.kt
+++ src/main/kotlin/com/bluedragonmc/server/bootstrap/PerInstanceChat.kt
@@ -1,6 +1,7 @@
 package com.bluedragonmc.server.bootstrap
 
 import net.minestom.server.event.Event
+import net.minestom.server.event.EventFilter
 import net.minestom.server.event.EventNode
 import net.minestom.server.event.player.PlayerChatEvent
 import net.minestom.server.event.player.PlayerDeathEvent
@@ -12,7 +13,12 @@ object PerInstanceChat : Bootstrap() {
             // Restrict player chat messages to only be visible in the player's current instance.
             event.recipients.removeAll { it.instance != event.player.instance }
         }
-        eventNode.addListener(PlayerDeathEvent::class.java) { event ->
+
+        val child = EventNode.event("per-instance-chat", EventFilter.ALL) { true }
+        child.priority = Integer.MAX_VALUE // High priority; runs last
+        eventNode.addChild(child)
+
+        child.addListener(PlayerDeathEvent::class.java) { event ->
             // Cancel the regular death message and only send it to the dead player's instance.
             val message = event.chatMessage
             event.chatMessage = null
