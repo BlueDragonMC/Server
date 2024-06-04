085ed07197e89004bb2e98cee88617a991b9fe85
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2024-06-04 04:54:15 -0400

    Update sign block handler for 1.20+

--- src/main/kotlin/com/bluedragonmc/server/bootstrap/GlobalBlockHandlers.kt
+++ src/main/kotlin/com/bluedragonmc/server/bootstrap/GlobalBlockHandlers.kt
@@ -5,57 +5,70 @@ import net.minestom.server.event.Event
 import net.minestom.server.event.EventNode
 import net.minestom.server.instance.block.BlockHandler
 import net.minestom.server.tag.Tag
+import net.minestom.server.tag.TagSerializer
 import net.minestom.server.utils.NamespaceID
 
 object GlobalBlockHandlers : Bootstrap() {
     override fun hook(eventNode: EventNode<Event>) {
-        registerHandler("minecraft:sign", listOf(
-            Tag.Byte("GlowingText"),
-            Tag.String("Color"),
-            Tag.String("Text1"),
-            Tag.String("Text2"),
-            Tag.String("Text3"),
-            Tag.String("Text4")
-        ))
-        registerHandler("minecraft:skull", listOf(
-            Tag.String("ExtraType"),
-            Tag.NBT("SkullOwner")
-        ))
-        registerHandler("minecraft:beacon", listOf(
-            Tag.Component("CustomName"),
-            Tag.String("Lock"),
-            Tag.Integer("Levels"),
-            Tag.Integer("Primary"),
-            Tag.Integer("Secondary")
-        ))
-        registerHandler("minecraft:furnace", listOf(
-            Tag.Short("BurnTime"),
-            Tag.Short("CookTime"),
-            Tag.Short("CookTimeTotal"),
-            Tag.Component("CustomName"),
-            Tag.ItemStack("Items").list(),
-            Tag.String("Lock"),
-            Tag.Integer("RecipesUsed").list()
-        ))
-        registerHandler("minecraft:banner", listOf(
-            Tag.String("CustomName"),
-            Tag.NBT("Patterns").list()
-        ))
-        registerHandler("minecraft:dropper", listOf(
-            Tag.String("CustomName"),
-            Tag.ItemStack("Items").list(),
-            Tag.String("Lock"),
-            Tag.String("LootTable"),
-            Tag.Long("LootTableSeed")
-        ))
+        registerHandler(
+            "minecraft:sign", listOf(
+                // https://minecraft.wiki/w/Sign#Block_data
+                Tag.Byte("is_waxed"),
+                Tag.NBT("front_text"),
+                Tag.NBT("back_text"),
+            )
+        )
+        registerHandler(
+            "minecraft:skull", listOf(
+                Tag.String("ExtraType"),
+                Tag.NBT("SkullOwner")
+            )
+        )
+        registerHandler(
+            "minecraft:beacon", listOf(
+                Tag.Component("CustomName"),
+                Tag.String("Lock"),
+                Tag.Integer("Levels"),
+                Tag.Integer("Primary"),
+                Tag.Integer("Secondary")
+            )
+        )
+        registerHandler(
+            "minecraft:furnace", listOf(
+                Tag.Short("BurnTime"),
+                Tag.Short("CookTime"),
+                Tag.Short("CookTimeTotal"),
+                Tag.Component("CustomName"),
+                Tag.ItemStack("Items").list(),
+                Tag.String("Lock"),
+                Tag.Integer("RecipesUsed").list()
+            )
+        )
+        registerHandler(
+            "minecraft:banner", listOf(
+                Tag.String("CustomName"),
+                Tag.NBT("Patterns").list()
+            )
+        )
+        registerHandler(
+            "minecraft:dropper", listOf(
+                Tag.String("CustomName"),
+                Tag.ItemStack("Items").list(),
+                Tag.String("Lock"),
+                Tag.String("LootTable"),
+                Tag.Long("LootTableSeed")
+            )
+        )
         registerHandler("minecraft:daylight_detector", listOf())
-        registerHandler("minecraft:chest", listOf(
-            Tag.String("CustomName"),
-            Tag.ItemStack("Items").list(),
-            Tag.String("Lock"),
-            Tag.String("LootTable"),
-            Tag.Long("LootTableSeed")
-        ))
+        registerHandler(
+            "minecraft:chest", listOf(
+                Tag.String("CustomName"),
+                Tag.ItemStack("Items").list(),
+                Tag.String("Lock"),
+                Tag.String("LootTable"),
+                Tag.Long("LootTableSeed")
+            )
+        )
     }
 
     private fun registerHandler(registryName: String, blockEntityTags: List<Tag<*>>) =
