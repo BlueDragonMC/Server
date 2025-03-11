6dd5a0471e0be5864f2d0f24d38f96b85b01aaac
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-03-11 01:42:01 -0400

    Add door opening and closing sounds

--- common/src/main/kotlin/com/bluedragonmc/server/module/vanilla/DoorsModule.kt
+++ common/src/main/kotlin/com/bluedragonmc/server/module/vanilla/DoorsModule.kt
@@ -2,13 +2,19 @@ package com.bluedragonmc.server.module.vanilla
 
 import com.bluedragonmc.server.Game
 import com.bluedragonmc.server.module.GameModule
+import net.kyori.adventure.sound.Sound
+import net.minestom.server.adventure.audience.PacketGroupingAudience
 import net.minestom.server.coordinate.Point
 import net.minestom.server.entity.Player
 import net.minestom.server.event.Event
 import net.minestom.server.event.EventNode
 import net.minestom.server.event.player.PlayerBlockInteractEvent
+import net.minestom.server.instance.EntityTracker
 import net.minestom.server.instance.Instance
 import net.minestom.server.instance.block.Block
+import net.minestom.server.item.Material
+import net.minestom.server.sound.SoundEvent
+import kotlin.random.Random
 
 /**
  * Adapted from BasicRedstone by TogAr2 under the MIT License
@@ -36,7 +42,7 @@ class DoorsModule : GameModule() {
         Block.WAXED_COPPER_DOOR,
         Block.WAXED_EXPOSED_COPPER_DOOR,
         Block.WAXED_WEATHERED_COPPER_DOOR,
-        Block.WAXED_OXIDIZED_COPPER,
+        Block.WAXED_OXIDIZED_COPPER_DOOR,
     )
 
     private fun isDoor(block: Block) = woodDoors.any { it.compare(block, Block.Comparator.ID) }
@@ -61,24 +67,50 @@ class DoorsModule : GameModule() {
             otherHalfPos, instance.getBlock(otherHalfPos).withProperty("open", open.toString())
         )
 
-        // TODO Play effect only if state changed
-        // MC 1.21.4 replaced EffectPacket with WorldEventPacket, but opening a door is not a world event...
-//        val shouldPlayEffect = playEffect && (block.getProperty("open").equals("true")) != open
-//
-//        if (shouldPlayEffect) {
-//            // Play an effect to nearby players
-//            val effect = if (open) Effects.WOODEN_DOOR_OPENED else Effects.WOODEN_DOOR_CLOSED
-//
-//            val audience = mutableListOf<Player>()
-//            instance.entityTracker.nearbyEntities(position, 64.0, EntityTracker.Target.PLAYERS) { player ->
-//                audience.add(
-//                    player
-//                )
-//            }
-//            PacketSendingUtils.sendGroupedPacket(
-//                audience, EffectPacket(effect.id, position, 0, false)
-//            ) { player -> player != source }
-//        }
+        val shouldPlaySound = playEffect && (block.getProperty("open").equals("true")) != open
+
+        if (shouldPlaySound) {
+            val (openSound, closeSound) = (getSounds(block.registry().material() ?: return) ?: return)
+            val soundEvent = if (open) openSound else closeSound
+
+            val audience = mutableListOf<Player>()
+            instance.entityTracker.nearbyEntities(position, 16.0, EntityTracker.Target.PLAYERS) { player ->
+                if (player != source) audience.add(player)
+            }
+            PacketGroupingAudience.of(audience).playSound(Sound.sound(soundEvent, Sound.Source.BLOCK, 1.0f, 0.9f + Random.nextFloat() * 0.1f), position)
+        }
+    }
+
+    fun getSounds(material: Material): Pair<SoundEvent, SoundEvent>? {
+        // See yarn: net.minecraft.block.BlockSetType or mojmap: net.minecraft.world.level.block.state.properties.BlockSetType
+        return when (material) {
+            Block.ACACIA_DOOR,
+            Block.BIRCH_DOOR,
+            Block.DARK_OAK_DOOR,
+            Block.JUNGLE_DOOR,
+            Block.MANGROVE_DOOR,
+            Block.SPRUCE_DOOR,
+            Block.OAK_DOOR -> SoundEvent.BLOCK_WOODEN_DOOR_OPEN to SoundEvent.BLOCK_WOODEN_DOOR_CLOSE
+
+            Block.CHERRY_DOOR -> SoundEvent.BLOCK_CHERRY_WOOD_DOOR_OPEN to SoundEvent.BLOCK_CHERRY_WOOD_DOOR_CLOSE
+
+            Block.CRIMSON_DOOR,
+            Block.WARPED_DOOR -> SoundEvent.BLOCK_NETHER_WOOD_DOOR_OPEN to SoundEvent.BLOCK_NETHER_WOOD_DOOR_CLOSE
+
+
+            Block.BAMBOO_DOOR -> SoundEvent.BLOCK_BAMBOO_WOOD_DOOR_OPEN to SoundEvent.BLOCK_BAMBOO_WOOD_DOOR_CLOSE
+
+            Block.COPPER_DOOR,
+            Block.EXPOSED_COPPER_DOOR,
+            Block.WEATHERED_COPPER_DOOR,
+            Block.OXIDIZED_COPPER_DOOR,
+            Block.WAXED_COPPER_DOOR,
+            Block.WAXED_EXPOSED_COPPER_DOOR,
+            Block.WAXED_WEATHERED_COPPER_DOOR,
+            Block.WAXED_OXIDIZED_COPPER_DOOR -> SoundEvent.BLOCK_COPPER_DOOR_OPEN to SoundEvent.BLOCK_COPPER_DOOR_CLOSE
+
+            else -> null
+        }
     }
 
     override fun initialize(parent: Game, eventNode: EventNode<Event>) {
