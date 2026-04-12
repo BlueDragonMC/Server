34872006d37f63f93ea0552a53033ca843f12e2a
ex4dev <31103658+ex4dev@users.noreply.github.com>
2026-04-12 11:42:56 -0400

    Add post-game rewards feature to AwardsModule

--- common/src/main/kotlin/com/bluedragonmc/server/module/database/AwardsModule.kt
+++ common/src/main/kotlin/com/bluedragonmc/server/module/database/AwardsModule.kt
@@ -1,9 +1,12 @@
 package com.bluedragonmc.server.module.database
 
 import com.bluedragonmc.server.*
+import com.bluedragonmc.server.event.PlayerLeaveGameEvent
 import com.bluedragonmc.server.model.PlayerDocument
 import com.bluedragonmc.server.service.Database
 import com.bluedragonmc.server.module.GameModule
+import com.bluedragonmc.server.module.minigame.SpectatorModule
+import com.bluedragonmc.server.module.minigame.WinModule
 import com.bluedragonmc.server.service.Messaging
 import com.bluedragonmc.server.utils.*
 import kotlinx.coroutines.launch
@@ -21,27 +24,43 @@ import java.time.Duration
 class AwardsModule : GameModule() {
 
     private lateinit var parent: Game
+    private val postGameAwards = mutableMapOf<PlayerAwardReason, Int>()
+
+    private data class PlayerAwardReason(val player: Player, val reason: Component)
 
     override fun initialize(parent: Game, eventNode: EventNode<Event>) {
         this.parent = parent
+
+        eventNode.addListener(PlayerLeaveGameEvent::class.java) { event ->
+            distributePostGameAwards(event.player)
+        }
+
+        eventNode.addListener(SpectatorModule.StartSpectatingEvent::class.java) { event ->
+            distributePostGameAwards(event.player)
+        }
+
+        eventNode.addListener(WinModule.WinnerDeclaredEvent::class.java) { _ ->
+            parent.players.forEach { player ->
+                distributePostGameAwards(player)
+            }
+        }
     }
 
     fun awardCoins(player: Player, amount: Int, reason: Component) {
-        player as CustomPlayer
-        require(player.isDataInitialized()) { "Player's data has not loaded!" }
-        val oldLevel = CustomPlayer.getXpLevel(player.data.experience).toInt()
-        Database.IO.launch {
-            player.data.compute(PlayerDocument::coins) { it + amount }
-            player.data.compute(PlayerDocument::experience) { it + amount }
-            Messaging.outgoing.recordCoinAward(player.uuid, amount, parent.id)
-            val newLevel = CustomPlayer.getXpLevel(player.data.experience).toInt()
-            if (newLevel > oldLevel)
-                MinecraftServer.getSchedulerManager().buildTask { notifyLevelUp(player, oldLevel, newLevel) }
-                    .delay(Duration.ofSeconds(2)).schedule()
+        addCoins(player, amount)
+        sendAwardMessage(player, amount, reason)
+    }
+
+    /**
+     * Distributes the specified award when the game ends or the player is eliminated (they leave or become a spectator).
+     */
+    fun awardCoinsAfterGame(player: Player, amount: Int, reason: Component) {
+        if (parent.state == GameState.ENDING) {
+            awardCoins(player, amount, reason)
+            return
         }
-        player.sendMessage(
-            Component.translatable("module.award.awarded_coins", ALT_COLOR_2, Component.text(amount), reason)
-        )
+        val reasonStruct = PlayerAwardReason(player, reason)
+        postGameAwards[reasonStruct] = postGameAwards.getOrDefault(reasonStruct, 0) + amount
     }
 
     private fun notifyLevelUp(player: CustomPlayer, oldLevel: Int, newLevel: Int) {
@@ -58,5 +77,43 @@ class AwardsModule : GameModule() {
         }
         player.sendMessage(msg.surroundWithSeparators())
         player.playSound(Sound.sound(SoundEvent.ENTITY_PLAYER_LEVELUP, Sound.Source.PLAYER, 1.0F, 1.0F))
+    }
+
+    private fun addCoins(player: Player, amount: Int) {
+        player as CustomPlayer
+        require(player.isDataInitialized()) { "Player's data has not loaded!" }
+        val oldLevel = CustomPlayer.getXpLevel(player.data.experience).toInt()
+        Database.IO.launch {
+            player.data.compute(PlayerDocument::coins) { it + amount }
+            player.data.compute(PlayerDocument::experience) { it + amount }
+            Messaging.outgoing.recordCoinAward(player.uuid, amount, parent.id)
+            val newLevel = CustomPlayer.getXpLevel(player.data.experience).toInt()
+            if (newLevel > oldLevel)
+                MinecraftServer.getSchedulerManager().buildTask { notifyLevelUp(player, oldLevel, newLevel) }
+                    .delay(Duration.ofSeconds(2)).schedule()
+        }
+    }
+
+    private fun sendAwardMessage(player: Player, amount: Int, reason: Component) {
+        player.sendMessage(
+            Component.translatable("module.award.awarded_coins", ALT_COLOR_2, Component.text(amount), reason)
+        )
+    }
+
+    private fun distributePostGameAwards(player: Player) {
+        synchronized(postGameAwards) {
+            var total = 0
+            postGameAwards.entries.removeAll { (award, amount) ->
+                if (award.player != player) return@removeAll false
+                total += amount
+                sendAwardMessage(award.player, amount, award.reason)
+                true
+            }
+            addCoins(player, total)
+        }
+    }
+
+    companion object {
+        val AWARD_REASON_KILLS = Component.translatable("module.award.reason.kills")
     }
 }
