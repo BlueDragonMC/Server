5947b6ae58953f5075676a52e23d27cb2ee0a5db
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2026-06-13 19:08:40 -0400

    Switch to new custom Reposilite Maven repo

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
     id("server.common-conventions")
+    id("org.jetbrains.dokka-javadoc") version "2.2.0"
     kotlin("plugin.serialization") version "2.3.0"
     `maven-publish`
 }
 
 group = "com.bluedragonmc.server"
-version = "1.0-SNAPSHOT"
+version = rootProject.version
 
 repositories {
     mavenLocal()
     mavenCentral()
-    maven(url = "https://jitpack.io")
-    maven("https://reposilite.atlasengine.ca/public")
+    maven(url = "https://reposilite.bluedragonmc.com/releases")
 }
 
 dependencies {
@@ -27,14 +27,41 @@ dependencies {
     implementation(libs.fastutil)
 }
 
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets.main.get().allSource)
+}
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaGeneratePublicationJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
 publishing {
+    val rootVersion = rootProject.version.toString()
+    val inCI = rootVersion != "dev"
+    repositories {
+        if (inCI) {
+            maven {
+                name = "reposilite"
+                url = uri("https://reposilite.bluedragonmc.com/releases")
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
     publications {
         create<MavenPublication>("maven") {
             groupId = "com.bluedragonmc.server"
             artifactId = "common"
-            version = "1.0"
+            version = rootVersion
 
             from(components["java"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
         }
     }
 }
